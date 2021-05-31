3d4f8d131037ed6a700a40bffaa5d2ae1713529a
hirasakavizu <havisikkubaru@gmail.com>
2021-05-31 23:59:06 +0700

    Refactor Format Rupiah

--- application/views/template.php
+++ application/views/template.php
@@ -138,48 +138,28 @@
             });
         });
 
-        var rupiah1 = document.getElementById("rupiah1");
-        var rupiah2 = document.getElementById("rupiah2");
-
-        rupiah1.addEventListener("keyup", function(e) {
-          rupiah1.value = formatRupiah(this.value, "Rp. ");
-        });
-
-        function formatRupiah(angka, prefix) {
-          var number_string = angka.replace(/[^,\d]/g, "").toString(),
-            split = number_string.split(","),
-            sisa = split[0].length % 3,
-            rupiah1 = split[0].substr(0, sisa),
-            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-          if (ribuan) {
-            separator = sisa ? "." : "";
-            rupiah1 += separator + ribuan.join(".");
-          }
-
-          rupiah1 = split[1] != undefined ? rupiah1 + "," + split[1] : rupiah1;
-          return prefix == undefined ? rupiah1 : rupiah1 ? "Rp. " + rupiah1 : "";
-        }
-
-        rupiah2.addEventListener("keyup", function(e) {
-          rupiah2.value = formatRupiah(this.value, "Rp. ");
-        });
+        var rupiah = $("#rupiah1");
+        
+        rupiah.keyup((e) => {
+          rupiah.val(formatRupiah(e.target.value, "Rp. "));
+        })
 
         function formatRupiah(angka, prefix) {
           var number_string = angka.replace(/[^,\d]/g, "").toString(),
             split = number_string.split(","),
             sisa = split[0].length % 3,
-            rupiah2 = split[0].substr(0, sisa),
+            rupiah = split[0].substr(0, sisa),
             ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
           if (ribuan) {
             separator = sisa ? "." : "";
-            rupiah2 += separator + ribuan.join(".");
+            rupiah += separator + ribuan.join(".");
           }
 
-          rupiah2 = split[1] != undefined ? rupiah2 + "," + split[1] : rupiah2;
-          return prefix == undefined ? rupiah2 : rupiah2 ? "Rp. " + rupiah2 : "";
+          rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
+          return prefix == undefined ? rupiah : rupiah ? "Rp. " + rupiah : "";
         }
+
     </script>
 
 </body>
